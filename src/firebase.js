3dcd338b0bf4122d4357b804070e123bf7cad5b0
Pongnut Jittipanyakul <pongnut17@hotmail.com>
2017-10-27 15:46:24 +0700

    Apply Vuetify

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyCOvFBb92U9s0uTpRESQfzn2H0f8wSvuNg",
+  authDomain: "ls-media-store.firebaseapp.com",
+  databaseURL: "https://ls-media-store.firebaseio.com",
+  projectId: "ls-media-store",
+  storageBucket: "ls-media-store.appspot.com",
+  messagingSenderId: "524937624947"
+};
+firebase.initializeApp(config);
