b53a746c2f5d0ffffa1fd23c2236d70ebbb19130
Pongnut Jittipanyakul <pongnut17@hotmail.com>
2017-12-09 02:49:04 +0700

    Init Vue.js + Firebase

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,112 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      persistent
+      clipped
+      app
+      v-model="drawer"
+    >
+      <v-list dense>
+        <template v-for="(item, i) in items">
+          <v-layout
+            row
+            v-if="item.heading"
+            align-center
+            :key="i"
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+            <v-flex xs6 class="text-xs-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-flex>
+          </v-layout>
+          <v-list-group v-else-if="item.children" v-model="item.model" no-action>
+            <v-list-tile slot="item" @click="">
+              <v-list-tile-action>
+                <v-icon>{{ item.model ? item.icon : item['icon-alt'] }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ item.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile
+              v-for="(child, i) in item.children"
+              :key="i"
+              @click=""
+            >
+              <v-list-tile-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ child.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-group>
+          <v-list-tile v-else @click="">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      color="deep-orange"
+      dark
+      app
+      clipped-left
+      fixed
+    >
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-avatar size="32px" tile>
+          <img
+            src="./assets/LS-logo-full.png"
+            alt="LS Media Store"
+            style="width: auto; height: 32px; padding-left: 120px;"
+          >
+        </v-avatar>
+      </v-toolbar-title>
+      <v-text-field
+        solo
+        prepend-icon="search"
+        placeholder="Search"
+      ></v-text-field>
+      <v-btn icon @click.stop="!login">
+        <v-icon>account_circle</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <main>
+
+    </main>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  data: () => ({
+    login: false,
+    drawer: false,
+    items: [
+      { icon: 'music_note', text: 'Music' },
+      { icon: 'movie', text: 'Movies' },
+      { icon: 'settings', text: 'Settings' }
+    ]
+  }),
+  props: {
+    source: String
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
+import * as firebase from 'firebase'
+
+// index.js or main.js
+import('../node_modules/vuetify/dist/vuetify.min.css') // Ensure you are using css-loader
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+  apiKey: 'AIzaSyCOvFBb92U9s0uTpRESQfzn2H0f8wSvuNg',
+  authDomain: 'ls-media-store.firebaseapp.com',
+  databaseURL: 'https://ls-media-store.firebaseio.com',
+  projectId: 'ls-media-store',
+  storageBucket: 'ls-media-store.appspot.com',
+  messagingSenderId: '524937624947'
+}
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
